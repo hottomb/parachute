a8054cb9e5e3c775ab49d3d992f86b5833b8c3bc
Unknown <izochromy@gmail.com>
2017-05-10 16:31:49 +0200

    Initial commit

---
+++ spad.m
@@ -0,0 +1,36 @@
+clear all;
+load dc2_pp.mat;
+
+r=1           %promien polczaszy spadochronu
+g=9.81;         %przyciaganie ziemskie
+Cd=0.45;        %wsp. oporu czaszy polsferycznej
+Ro=1.168;       %gestosc powietrza
+gram=0.026;
+Sc=@(r)pi*r.^2;  %powierzchnia podstawy czaszy polsferycznej
+Sf=@(r)2*pi*r.^2;
+m_spad=@(r,gram)gram*Sf(r); %gramatura * polpowierzchnia sfery = masa czaszy
+t_vel=@(m,r)sqrt(2*g*m'./(Cd*Ro*Sc(r))); %predkosc opadania
+
+r1=1;
+r2=6;
+a=-2;
+    R=linspace(-r2^(1/-a),-r1^(1/-a),10).^a;
+    R=R*r2;
+    m_pay=0.0:0.05:10;
+%     figure(1);
+    Mx=NaN(2,1);My=Mx;
+for i=1:length(R)
+    r=R(i);
+    vels=t_vel(m_pay+m_spad(r,gram),r);
+
+    plot(m_pay,vels,'DisplayName',['R[m]= ' num2str(r,'%10.1f') '      fabr.m[g]= ' num2str(round(m_spad(r,gram)*1000)) '  fab.surf.[m^2]= ' num2str(Sf(r),'%10.1f') ]);hold on;
+    My(1:2)=[min([My(1);vels]);max([My(2);vels])];
+    Mx(1:2)=[min([Mx(1);m_pay']),max([Mx(2);m_pay'])];
+end
+
+title({'Terminal velocity vs payload mass vs sph. parachute cross section surface', 'Cd=0.45; Ro=1.168kg/m^3; fabric gramature=26g/m^2   '});
+xlabel('payload mass [kg]'); ylabel('velocity [m/s]');
+ylim([My(1) My(2)]); xlim([Mx(1) Mx(2)]);
+% set(gca,'XMinorTick','on','YMinorTick','on')
+set(gca,'XMinorGrid','on','YMinorGrid','on')
+legend('show');
